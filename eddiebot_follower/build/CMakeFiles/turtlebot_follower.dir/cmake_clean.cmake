989bbed5bde2669f370e672b4a1530a207bb0c12
robotictang <tang.tiong.yew@gmail.com>
2013-01-03 09:46:41 +0800

    Initial commit

---
+++ eddiebot_follower/build/CMakeFiles/turtlebot_follower.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/turtlebot_follower.dir/src/follower.o"
+  "../lib/libturtlebot_follower.pdb"
+  "../lib/libturtlebot_follower.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/turtlebot_follower.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ eddiebot_follower/follower.app
@@ -0,0 +1,5 @@
+display: Follower
+description: Take the EddieBot for a stroll. EddieBot will follow whatever is in front of it.
+platform: eddiebot
+launch: eddiebot_follower/follower.launch
+interface: eddiebot_follower/follower.interface
